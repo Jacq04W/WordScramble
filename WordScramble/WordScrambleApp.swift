07fbe598418c680e53563323a80a4e5706b7d57d
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-07-11 18:03:21 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jacquese Whitson on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
